cbd7d3980026c3767b88616d8f47a71aa386567b
Petro Krasnomovets <zhyrok@gmailc>
2018-06-20 14:27:46 +0200

    Refactored. Readme Added

--- src/client/components/App.js
+++ src/client/components/App.js
@@ -1,12 +1,13 @@
-import React, { Component } from 'react';
-import CasesTable from './my-custodianship/CasesTable';
-import ErrorBoundary from './utils/ErrorBoundary';
+import React, { Component } from "react";
+import CasesTable from "./my-custodianship/CasesTable";
+import ErrorBoundary from "./utils/ErrorBoundary";
 
 export default class App extends Component {
   render() {
     return (
       <ErrorBoundary>
         <CasesTable />
-      </ErrorBoundary>);
+      </ErrorBoundary>
+    );
   }
 }
